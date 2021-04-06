e117b6cf5d1f99707883c882a8cef74bcd4cde81
Tin Lai <oscar@tinyiu.com>
2021-04-06 18:07:08 +1000

    lower peek height
    Signed-off-by: Tin Lai <oscar@tinyiu.com>

--- app/src/main/java/soraxas/taskw/ui/TaskDetailActivity.kt
+++ app/src/main/java/soraxas/taskw/ui/TaskDetailActivity.kt
@@ -4,21 +4,22 @@ import android.content.Context
 import android.view.View
 import com.afollestad.materialdialogs.MaterialDialog
 import com.afollestad.materialdialogs.bottomsheets.BottomSheet
+import com.afollestad.materialdialogs.bottomsheets.setPeekHeight
 import com.afollestad.materialdialogs.customview.customView
 import soraxas.taskw.R
-import java.util.concurrent.TimeUnit
 
 fun showD(context: Context, view: View): MaterialDialog {
     return MaterialDialog(context, BottomSheet()).show {
-                //        title(text = "Your Title")
+        //        title(text = "Your Title")
 //        message(text = "Your Message")
 
 
 //        customView(R.layout.item_one_task)
 
+        setPeekHeight(1300)
 
-            customView(view = view)
-            // Using a dimen instead is encouraged as it's easier to have all instances changeable from one place
-            cornerRadius(res = R.dimen.bottomsheets_corner_radius)
-        }
+        customView(view = view)
+        // Using a dimen instead is encouraged as it's easier to have all instances changeable from one place
+        cornerRadius(res = R.dimen.bottomsheets_corner_radius)
+    }
 }
